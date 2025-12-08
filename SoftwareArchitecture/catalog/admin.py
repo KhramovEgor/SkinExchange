a0da8eb193e0b8b28bd2d99a416a736cf5559526
snidalap <shishov.zahar79@gmail.com>
2025-12-08 20:29:58 +0300

    fix: ╨╕╤Б╨┐╤А╨░╨▓╨╕╨╗ ╤Б╤В╤А╤Г╨║╤В╤Г╤А╤Г ╤А╨╡╨┐╨╛╨╖╨╕╤В╨╛╤А╨╕╤П, ╨┤╨╛╨▒╨░╨▓╨╕╨╗ .gitignore

--- SoftwareArchitecture/catalog/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Skin
-
-@admin.register(Skin)
-class SkinAdmin(admin.ModelAdmin):
-    list_display = ('name', 'type', 'rarity', 'price', 'price_updated')
-    list_filter = ('type', 'rarity')
-    search_fields = ('name', 'skin_id')
--- SoftwareArchitecture/inventory/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import UserSkin
-
-@admin.register(UserSkin)
-class UserSkinAdmin(admin.ModelAdmin):
-    list_display = ('user', 'skin', 'asset_id')
-    list_filter = ('user',)
-    search_fields = ('user__username', 'skin__name', 'asset_id')
